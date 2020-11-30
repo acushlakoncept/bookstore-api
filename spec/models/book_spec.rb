29fa3fd98e8084afdf7b9f2bf279564edb008585
Uduak Essien <acushla4real@gmail.com>
2020-11-30 15:17:20 +0100

    add test for category and book model\

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should have_one(:category) }
+
+  # Validation tests
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:author) }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  it { should belong_to(:book) }
+
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 end
