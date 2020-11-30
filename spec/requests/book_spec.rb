5d9cc87f83d38368d5ec6fa6ee94128d69ca4aef
Uduak Essien <acushla4real@gmail.com>
2020-11-30 15:42:47 +0100

    add faker input for books and category

---
+++ spec/requests/book_spec.rb
@@ -0,0 +1,8 @@
+# spec/factories/todos.rb
+FactoryBot.define do
+    factory :book do
+      title { Faker::Book.title }
+      created_by { Faker::Book.author }
+      category_id nil
+    end
+  end
