2c2b943ff8e936e9aa7eb9a0924f59e9bf8a2a87
Uduak Essien <acushla4real@gmail.com>
2020-12-01 03:34:03 +0100

    Add category controller

--- app/controllers/api/v1/category_controller.rb
+++ app/controllers/api/v1/category_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
-class CategoryController < ApplicationController
+module Api::V1
+  class CategoryController < ApplicationController
+    before_action :set_category, only: %i[show]
+
+    # GET /category
+
+    def index
+      @categories = Category.all
+      render json: @categories, except: %i[created_at updated_at]
+    end
+
+    # GET /category/:id
+    def show
+      render json: @category, except: %i[created_at updated_at]
+    end
+
+    private
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+  end
 end
